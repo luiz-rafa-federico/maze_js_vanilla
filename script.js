ca0b96f25cc7ee9e29363f9c2d3505149e9203ef
luizrafa-federico <luizrafa.federico@gmail.com>
2021-06-22 14:45:10 -0500

    acabei

--- script.js
+++ script.js
@@ -17,15 +17,10 @@ const map = [
 ];
 
 const criarLabirinto = () => {
-
-    //Criando labirinto
-    let labirinto = [];
-    let cell;
-    let divLinha;
-
+    
     for (let i = 0; i < map.length; i++) {
         const display = document.getElementsByTagName('body')[0];
-        divLinha = i;
+        let divLinha = i;
         divLinha = document.createElement('div');
         divLinha.style.display = 'flex';
         divLinha.classList.add('divLinha');
@@ -34,57 +29,76 @@ const criarLabirinto = () => {
         
         for(let j = 0; j < map[i].length; j++) {
             let mapa = map[i][j];
-            cell = map[i][j];
+            let cell = map[i][j];
             cell = document.createElement('div');
             divLinha.appendChild(cell);
 
             if (mapa == " ") {
-                cell.innerHTML = "P";
-                cell.style.visibility = 'hidden';
                 cell.classList.add('space')
-                cell.dataset.position = j;
+                cell.dataset.linha = `${i}`;
+                cell.dataset.coluna = `${j}`;
             } else if (mapa == 'W') {
                 cell.classList.add('wall')
-                cell.dataset.position = 'W';
+                cell.dataset.linha = `${i}`;
+                cell.dataset.coluna = `${j}`;
             } else if (mapa == 'S') {
                 cell.classList.add('start');
-                cell.id = 'player'
-                cell.dataset.position = j;
+                cell.dataset.linha = `${i}`;
+                cell.dataset.coluna = `${j}`;
             } else if (mapa == 'F') {
                 cell.classList.add('finish');
-                cell.dataset.position = j;
+                cell.dataset.linha = `${i}`;
+                cell.dataset.coluna = `${j}`;
             }
         }
-        labirinto.push(divLinha);
     }
 
-    //Movendo jogador
-    let playerTop = 0;
-    let playerLeft = 0;
-    let keyName;
-    
-    let playerElm = document.getElementById('player');
-    let player = labirinto[9].firstElementChild;
-    let playerPosition = [];
-    playerPosition[0] = labirinto[9][0];
-
     document.addEventListener('keydown', (evt) => {
-        keyName = evt.key;
-        
+        let keyName = evt.key;
+        const jogador = document.querySelector('.start');
+        const posicaoAtualLinha = Number(jogador.dataset.linha);
+        const posicaoAtualColuna = Number(jogador.dataset.coluna);
+        let proxPosicaoLinha = 0;
+        let proxPosicaoColuna = 0;
+
         if (keyName === 'ArrowDown') {
-            playerTop += 30;
+            proxPosicaoLinha = posicaoAtualLinha + 1;
+            proxPosicaoColuna = posicaoAtualColuna;
         } else if (keyName === 'ArrowUp') {
-            playerTop -= 30;
+            proxPosicaoLinha = posicaoAtualLinha - 1;
+            proxPosicaoColuna = posicaoAtualColuna;
         }
         if (keyName === 'ArrowRight') {
-            playerLeft += 30;
+            proxPosicaoLinha = posicaoAtualLinha;
+            proxPosicaoColuna = posicaoAtualColuna + 1;
         } else if (keyName === 'ArrowLeft') {
-            playerLeft -= 30;
+            proxPosicaoLinha = posicaoAtualLinha;
+            proxPosicaoColuna = posicaoAtualColuna - 1;
         }
-        playerElm.style.top = playerTop + 'px';
-        playerElm.style.left = playerLeft + 'px';
+        const linha = document.querySelectorAll('.divLinha');
+        
+        for (let i = 0; i < linha.length; i++) {
+            const line = linha[i].childNodes;
+            for (let j = 0; j < line.length; j++) {
+                const bloco = line[j];
+                const positionLinha = Number(bloco.dataset.linha);
+                const positionColuna = Number(bloco.dataset.coluna);
+                if (proxPosicaoLinha === positionLinha && proxPosicaoColuna === positionColuna && bloco.classList.value !== 'wall') {
+                    bloco.classList.remove('space');
+                    bloco.classList.add('start');
+                    jogador.classList.remove('start')
+                    jogador.classList.add('space')
+                }
+            }
+        }
+        if (proxPosicaoLinha === 8 && proxPosicaoColuna === 20) {
+            alert("Parabéns, você venceu!")
+            const btn = document.getElementById('btn');
+            btn.style.display = 'block';
+            btn.addEventListener('click', () => {
+                location.reload()
+            });
+        }
     });
 }
 criarLabirinto();
-
-
